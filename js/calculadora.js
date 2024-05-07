506e88a84772e2b2d0be7d3b7a1b5c3528afe6b5
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2024-05-07 06:33:41 -0300

    Atualização Calculadora - Troca de sinais

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,42 +1,54 @@
+var resultado = '';
+
 function inserir(num){
     var display = document.getElementById('display').innerHTML;
 
-    if (display.length <= 19){
-        document.getElementById('display').innerHTML = display+num;
+    if (resultado.toString().length <= 19){
+        resultado = resultado+num;
+        if (num == '*'){
+            document.getElementById('display').innerHTML = display + 'x';
+        }else if (num == '/'){
+            document.getElementById('display').innerHTML = display + '÷';
+        }else if (num == '.'){
+            document.getElementById('display').innerHTML = display + ',';
+        }else{
+            document.getElementById('display').innerHTML = display + num;
+        }
+    }else{
+        document.getElementById('display').innerHTML = resultado.length;
     }
 
 }
 
 function limpar(){
     document.getElementById('display').innerHTML =  "";
+    resultado = '';
 }
 
 function voltar(){
     var display = document.getElementById('display').innerHTML;
 
     document.getElementById('display').innerHTML = display.substring(0, display.length -1);
+    resultado = resultado.substring(0, display.length -1);
 }
 
 function calcular(){
-    var display = document.getElementById('display').innerHTML;
-
-    if (display){
-        document.getElementById('display').innerHTML = eval(display);
+    if (resultado){
+        document.getElementById('display').innerHTML = eval(resultado).toString().replace('.', ',');
+        resultado = eval(resultado);
     }
 }
 
 function raiz(){
-    var display = document.getElementById('display').innerHTML;
-
-    if (display){
-        document.getElementById('display').innerHTML = Math.sqrt(eval(display));
+    if (resultado){
+        document.getElementById('display').innerHTML = Math.sqrt(eval(resultado));
+        resultado = Math.sqrt(eval(resultado));
     }
 }
 
 function potencia(){
-    var display = document.getElementById('display').innerHTML;
-
-    if (display){
-        document.getElementById('display').innerHTML = eval(display)**2;
+    if (resultado){
+        document.getElementById('display').innerHTML = eval(resultado)**2;
+        resultado = eval(resultado)**2;
     }
 }
